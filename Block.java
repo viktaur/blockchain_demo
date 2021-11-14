6ccbee9c9808ceba27f201aa1b55bc236e1b6081
viktaur <victorgabal@gmail.com>
2021-11-14 18:46:15 +0000

    many changes

--- Block.java
+++ Block.java
@@ -7,7 +7,7 @@ public class Block {
   String previousHash;
   String data;
   String validBlockHash;
-  int nonce;
+  int validNonce;
 
   public Block(int id, String previousHash, String data) {
     this.id = id;
@@ -19,8 +19,17 @@ public class Block {
     return id;
   }
 
-  public String getBlockHash() {
+  public String getBlockHash(int nonce) {
     return Toolbox.getHash(getData() + getPreviousHash() + nonce);
+  }
+
+  public String getValidBlockHash() {
+    mineBlock();
+    return validBlockHash;
+  }
+
+  public int getValidNonce() {
+    return validNonce;
   }
 
   public String getPreviousHash() {
@@ -31,17 +40,30 @@ public class Block {
     return data;
   }
 
-  public boolean isValid() {
-    return getBlockHash().startsWith("0000");
+  public boolean isValid(int nonce) {
+    return getBlockHash(nonce).startsWith("0000");
   }
 
   public void mineBlock(){
     for (int i = 0; i < maximumNonce; i++) {
-      if (isValid()) {
-        nonce = i;
-        validBlockHash = getBlockHash();
+      if (isValid(i)) {
+        validBlockHash = getBlockHash(i);
+        validNonce = i;
+        System.out.println("The nonce is " + validNonce);
         break;
+      } else {
+        System.out.println(i);
       }
     }
+  }
+
+  public void printBlock() {
+    System.out.println("--------------------------------------------------------------------------------");
+    System.out.println("ID: " + id);
+    System.out.println("Hash: " + getBlockHash(validNonce));
+    System.out.println("Nonce: " + validNonce);
+    System.out.println("Previous hash: " + previousHash);
+    System.out.println("Valid: " + isValid(validNonce));
+    System.out.println("--------------------------------------------------------------------------------");
   }
 }
--- Main.java
+++ Main.java
@@ -3,13 +3,21 @@ import java.util.List;
 
 public class Main {
 
-  List<Block> blockchain = new ArrayList<>();
+  public static void main(String[] args) {
 
-  public void main(String[] args) {
+    List<Block> blockchain = new ArrayList<>();
 
-    blockchain.add(new Block(0, "0000000000000000000000000000000000000000000000000000000000000000", "genesis block"));
-    blockchain.add(new Block(1, blockchain.get(0).getBlockHash(), "data on block 1"));
-    blockchain.add(new Block(2, blockchain.get(1).getBlockHash(), "data on block 2 alsdkfjlksdj"));
+    Block block0 = new Block(0, "0000000000000000000000000000000000000000000000000000000000000000", "genesis block");
+
+    blockchain.add(block0);
+    blockchain.add(new Block(1, blockchain.get(0).getValidBlockHash(), "data on block 1"));
+    blockchain.add(new Block(2, blockchain.get(1).getValidBlockHash(), "data on block 2"));
+    blockchain.add(new Block(3, blockchain.get(2).getValidBlockHash(), "data on block 3"));
+
+    block0.printBlock();
+    blockchain.get(1).printBlock();
+    blockchain.get(2).printBlock();
+    blockchain.get(3).printBlock();
 
   }
 }
