3893eda462099a3da7a5b8981a7c3cadfbb13218
viktaur <victorgabal@gmail.com>
2021-11-14 12:09:41 +0000

    block 2

--- Main.java
+++ Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     blockchain.add(new Block(0, "0000000000000000000000000000000000000000000000000000000000000000", "genesis block"));
     blockchain.add(new Block(1, blockchain.get(0).getBlockHash(), "data on block 1"));
+    blockchain.add(new Block(2, blockchain.get(1).getBlockHash(), "data on block 2 alsdkfjlksdj"));
 
   }
 }
